9ba83cf6c3e83617947729b02d977e932bfb4160
gilcu3 <828241+gilcu3@users.noreply.github.com>
2018-10-08 11:40:00 -0400

    updated libraries and sdk dependencies removed RootShell dependency

--- app/src/main/java/uci/localproxy/proxycore/ProxyService.java
+++ app/src/main/java/uci/localproxy/proxycore/ProxyService.java
@@ -17,10 +17,7 @@ import android.widget.Toast;
 import com.bitbucket.lonelydeveloper97.wifiproxysettingslibrary.proxy_change_realisation.wifi_network.WifiProxyChanger;
 import com.bitbucket.lonelydeveloper97.wifiproxysettingslibrary.proxy_change_realisation.wifi_network.exceptions.ApiNotSupportedException;
 import com.bitbucket.lonelydeveloper97.wifiproxysettingslibrary.proxy_change_realisation.wifi_network.exceptions.NullWifiConfigurationException;
-import com.stericson.RootShell.exceptions.RootDeniedException;
-import com.stericson.RootShell.execution.Command;
-import com.stericson.RootShell.execution.Shell;
-import com.stericson.RootTools.RootTools;
+
 
 import java.io.IOException;
 import java.lang.reflect.Field;
@@ -175,109 +172,4 @@ public class ProxyService extends Service {
         startForeground(NOTIFICATION, notification);
     }
 
-
-    //This from <SandroProxy proyect>/projects/SandroProxyPlugin/src/org/sandroproxy/plugin/gui/MainActivity.java
-//    private static java.util.logging.Logger logger = java.util.logging.Logger.getLogger(ProxyService.class.getName());
-    private void ipTablesForTransparentProxy(boolean activate) {
-        int processId = getApplicationInfo().uid;
-        String excludedUid = String.valueOf(processId);
-        String action = (activate) ? "A" : "D";
-        String chainName = "spplugin";
-        String chainName1 = "sppluginOutput";
-        List<String> rules = new ArrayList<String>();
-
-        String r = "iptables -t nat -" + action + " OUTPUT -p 6 -d 10.0.0.1 -j RETURN";
-        String redirectRule = "iptables -t nat -" + action + " OUTPUT -p 6 --dport 80 -m owner ! --uid-owner " + excludedUid + " -j REDIRECT --to-port 8080 ";
-        String redirectRule2 = "iptables -t nat -" + action + " OUTPUT -p 6 --dport 443 -m owner ! --uid-owner " + excludedUid + " -j REDIRECT --to-port 8080 ";
-        String redirectRule3 = "iptables -t nat -" + action + " OUTPUT -p 6 --dport 5228 -m owner ! --uid-owner " + excludedUid + " -j REDIRECT --to-port 8080 ";
-
-        try {
-            Command command0 = new Command(4, r) {
-                @Override
-                public void commandOutput(int id, String line) {
-                    super.commandOutput(id, line);
-//                    Log.e("command output", line);
-                }
-            };
-            Command command = new Command(0, redirectRule);
-            Command command1 = new Command(1, redirectRule2);
-            Command command2 = new Command(1, redirectRule3);
-            Shell shell = RootTools.getShell(true);
-            shell.add(command0);
-            shell.add(command);
-            shell.add(command1);
-            shell.add(command2);
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (TimeoutException e) {
-            e.printStackTrace();
-        } catch (RootDeniedException e) {
-            e.printStackTrace();
-        }
-//        String redirectRule2 = "iptables -t nat -" + action + " OUTPUT -p 6 --dport 0:65535 -m owner ! --uid-owner " + excludedUid + " -j REDIRECT --to-port 8080 ";
-//        rules.add(redirectRule2);
-
-//        String rule0 = "iptables -" + action + " POSTROUTING -t nat -j MASQUERADE";
-//        String rule1 = "iptables -t nat -" + action + " OUTPUT -m owner --uid-owner " + excludedUid  + " -j ACCEPT";
-//        String rule2 = "iptables -t nat -" + action + " OUTPUT -p tcp --dport 80 -j REDIRECT --to-port 8080";
-//        String rule3 = "iptables -t nat -" + action + " OUTPUT -p tcp --dport 443 -j REDIRECT --to-port 8080";
-////        String rule3 = "iptables -t nat -" + action + " OUTPUT -p tcp --dport 80 -j DNAT --to :8080";
-//        rules.add(rule0);
-//        rules.add(rule1);
-//        rules.add(rule2);
-//        rules.add(rule3);
-
-
-//        if (activate){
-//            action = "A";
-//            String createChainRule = "iptables --new " + chainName; rules.add(createChainRule);
-//            String createNatChainRule = "iptables -t nat --new " + chainName; rules.add(createNatChainRule);
-//            String createNatChainRule1 = "iptables -t nat --new " + chainName1; rules.add(createNatChainRule1);
-//        }else{
-//            action = "D";
-//            String dettachChainRule = "iptables -D INPUT -j " + chainName; rules.add(dettachChainRule);
-//            String dettachNatChainRule = "iptables -t nat -D PREROUTING -j " + chainName; rules.add(dettachNatChainRule);
-//            String dettachNatChainRule1 = "iptables -t nat -D OUTPUT -j " + chainName1; rules.add(dettachNatChainRule1);
-//        }
-//
-
-//        Process p;
-//        try {
-//            p = Runtime.getRuntime().exec(new String[]{"su", "-c", "sh"});
-//
-//            DataOutputStream stdin = new DataOutputStream(p.getOutputStream());
-//            DataInputStream stdout = new DataInputStream(p.getInputStream());
-//            InputStream stderr = p.getErrorStream();
-//
-//            for (String rule : rules) {
-////                logger.finest(rule);
-//                stdin.writeBytes(rule + "\n");
-//                stdin.writeBytes("echo $?\n");
-//                Thread.sleep(100);
-//                byte[] buffer = new byte[4096];
-//                int read = 0;
-//                String out = new String();
-//                String err = new String();
-//                while (true) {
-//                    read = stdout.read(buffer);
-//                    out += new String(buffer, 0, read);
-//                    if (read < 4096) {
-//                        break;
-//                    }
-//                }
-//                while (stderr.available() > 0) {
-//                    read = stderr.read(buffer);
-//                    err += new String(buffer, 0, read);
-//                    if (read < 4096) {
-//                        break;
-//                    }
-//                }
-//                if (out != null && out.trim().length() > 0) Log.d(getClass().getName(), out);
-//                if (err != null && err.trim().length() > 0) Log.d(getClass().getName(), err);
-//            }
-//        } catch (Exception e) {
-//            e.printStackTrace();
-////            logger.finest("Error executing rules: " + e.getMessage());
-//        }
-    }
 }
